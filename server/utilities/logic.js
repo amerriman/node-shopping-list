79931836e44288be7c9ac1dab474e6f30a787a3c
amerriman <amm434@gmail.com>
2015-08-27 11:02:22 -0600

    took out for-in loop

--- server/utilities/logic.js
+++ server/utilities/logic.js
@@ -5,7 +5,6 @@ function handleDelete(itemID, storageArray) {
   if(listItem.length>0){
     for (var i = 0; i < storageArray.length; i++) {
       if(storageArray[i].id === parseInt(itemID)){
-
         var tempItem = storageArray.splice(i, 1);
         return {
           message: 'That item is off the list!',
@@ -20,10 +19,6 @@ function handleDelete(itemID, storageArray) {
 }
 
 
-//replace res.json with return
-//storage.items = storageArary
-// storage = storage
-//storage.addItem is a function from index...can I use that? with index.storage.addItem
 
 function handlePost(itemName, storage){
   var listItem = storage.items.filter(function(item){
@@ -42,9 +37,8 @@ function handlePost(itemName, storage){
     }
 }
 
-//itemID = req.params.id
-//body = req.body (can use it for rec.body.name, etc)
-//res.json gets replaced with return
+
+
 function handlePut(itemID, body, storage){
   var listItem = storage.items.filter(function(item){
     return item.id===parseInt(itemID);
@@ -52,16 +46,11 @@ function handlePut(itemID, body, storage){
   if(listItem.length>0){
     for (var i = 0; i < storage.items.length; i++) {
       if(storage.items[i].id === parseInt(itemID)){
-        for(var key in body){
-          if(key === "name") {
-            storage.items[i].name = body.name;
-          }
-        }
+        storage.items[i].name = body.name;
       }
     }
     return storage.items;
   } else {
-
     var newItem = storage.addItem(body.name);
       return({
         message: "Woot!",
@@ -69,8 +58,6 @@ function handlePut(itemID, body, storage){
       });
     }
 }
-
-
 
 
 
